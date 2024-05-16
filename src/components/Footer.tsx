29348b82a63dadb4bb9cde58e496066724a1c111
gitowa12 <118157881+gitowa12@users.noreply.github.com>
2024-05-16 19:42:59 +0900

    feat: footerの追加

---
+++ src/components/Footer.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <nav>
+        <ul className="mb-[24px] flex flex-col items-center">
+          <li className="font-bold tracking-wide text-lg w-full text-center border-y h-[48px] flex items-center justify-center">
+            <a href="#about">About</a>
+          </li>
+          <li className="font-bold tracking-wide text-lg w-full text-center border-b h-[48px] flex items-center justify-center">
+            <a href="#work">Work</a>
+          </li>
+          <li className="font-bold tracking-wide text-lg w-full text-center border-b h-[48px] flex items-center justify-center">
+            <a href="#blog">Blog</a>
+          </li>
+          <li className="font-bold tracking-wide text-lg w-full text-center border-b h-[48px] flex items-center justify-center">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="mb-[24px] flex justify-between items-center">
+        <p className="leading-none text-[12px] text-gray-600">©towa</p>
+        <div className="flex">
+          <a className="mr-[8px]" href="#">
+            <img src="/x.svg" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.svg" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
